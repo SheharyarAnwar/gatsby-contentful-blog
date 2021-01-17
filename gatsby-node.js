23d06c9b4e5b32ea559ac30054209f3fd3eaf424
Sherry <sheharyar28@hotmail.com>
2021-01-17 13:55:51 +0500

    webpackconfiguration to ignore firebase during ssr

--- gatsby-node.js
+++ gatsby-node.js
@@ -32,3 +32,17 @@ exports.createPages = async ({ graphql, actions }) => {
     });
   });
 };
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /firebase/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    });
+  }
+};
